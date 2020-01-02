2d1a4e7e1ffaee4765e19cecc18ae7601f108a5a
Jeremy Ondrey <jeremyondrey@gmail.com>
2020-01-02 17:36:49 +0100

    added basic mine script, removed some comments, happy new year!

---
+++ Assets/Scripts/MineScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineScript : MonoBehaviour
+{
+
+
+    public GameObject screw1;
+    public GameObject screw2;
+    public GameObject fuse;
+    public int mineState=0;
+
+    void Update()
+    {
+
+    switch (mineState)
+    {
+    case 1:
+        //first screw removed
+        screw1.SetActive(false);
+        break;
+    case 2:
+        //second screw removed
+        screw2.SetActive(false);
+        break;
+    case 3:
+        //mine hit 3 times with hammer
+        //todo: turn on rigidbody for fuse
+        break;
+        default:
+        break;
+    }
+
+    }
+}
--- Assets/Scripts/ToolSwitchReceiver.cs
+++ Assets/Scripts/ToolSwitchReceiver.cs
@@ -26,35 +26,30 @@ public class ToolSwitchReceiver : MonoBehaviour
         switch (toolID)
         {
         case 1:
-            print ("tool 1");
             detector.SetActive(true);
             stick.SetActive(false);
             screwdriver.SetActive(false);
             knife.SetActive(false);
             break;
         case 2:
-            print ("Hello and good day!");
             detector.SetActive(false);
             stick.SetActive(true);
             screwdriver.SetActive(false);
             knife.SetActive(false);
             break;
         case 3:
-            print ("Whadya want?");
             detector.SetActive(false);
             stick.SetActive(false);
             screwdriver.SetActive(true);
             knife.SetActive(false);
             break;
         case 4:
-            print ("Grog SMASH!");
             detector.SetActive(false);
             stick.SetActive(false);
             screwdriver.SetActive(false);
             knife.SetActive(true);
             break;
         default:
-            print ("Incorrect intelligence level.");
             break;
         }
     }
