541cfc4fe14a434e05c0202ee4102ffa1098062a
Jeremy Ondrey <jeremyondrey@gmail.com>
2020-01-03 19:31:01 +0100

    added button to mine pressure plate - doesn't really work properly though

--- Assets/Scripts/DebugScript.cs
+++ Assets/Scripts/DebugScript.cs
@@ -7,12 +7,12 @@ public class DebugScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Debug.Log("hey i have been pressed"); 
-    
+        
     }
 
     public void OnTriggered()
     {
+       Debug.Log("hey i have been pressed"); 
     }
 
     // Update is called once per frame
