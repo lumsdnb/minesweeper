fde071a66d24cfd51a0fbbe7e3c89e1345c09622
Jeremy Ondrey <jeremyondrey@gmail.com>
2020-01-20 11:34:40 +0100

    updated screw script

--- Assets/Scripts/ScrewThis.cs
+++ Assets/Scripts/ScrewThis.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //add rotation of screwdriver to rotation of screw if their colliders are touching
+//TODO: detect rotation past 360 degrees
 
 public class ScrewThis : MonoBehaviour
 {
-
-    public float speed = 10f;
-    public float angleRotated = 45;
-    private int rotations = 0;
+    public float angleRotated = 0;
+    public int rotations = 0;
     private float prevAngle = 0;
     private GameObject screwdriver;
     private Vector3 rotVec;
@@ -25,31 +24,29 @@ public class ScrewThis : MonoBehaviour
     {
         if(other.gameObject.tag == "screwdriver")
         {
-            rotVec = screwdriver.transform.rotation.eulerAngles;
-            rotVec = new Vector3(90,0,rot.z);
-
-            Debug.Log("angle: " + rotVec);
+            rotVec = new Vector3(0,0,screwdriver.transform.localRotation.eulerAngles.z);
+            //angleRotated = screwdriver.transform.rotation.eulerAngles.z;
+            //angleRotated = angleRotated + rotVec.z;
+            Debug.Log("angle: " + rotVec + "rotations: " + rotations);
             
-            angleRotated = rotVec.z;
         } 
     }
 
     void Update()
     {
-        prevAngle = angleRotated; 
+        prevAngle = rotVec.z; 
         //rotate exact amount in degrees
-        transform.rotation = Quaternion.Euler(rotVec);
+        transform.localRotation = Quaternion.Euler(0,rotVec.z,0);
         //if rotation goes past 360 ie back to 0
 
-        if (angleRotated < prevAngle)
+        if (rotVec.z < prevAngle)
         {
-            rotations++;
+            Debug.Log("one rotation");
+            //rotations++;
         }
-        //TODO: replace auto rotation with screwdriver input
-        //transform.Rotate (0, angleRotated * Time.deltaTime, 0);
         //transform.Translate(0, -angleRotated*(Time.deltaTime/40000), 0);
 
-        if (rot.z > 900)
+        if (rotVec.z > 270)
         {
             //screw falls out after x degrees of unscrewing
             gameObject.GetComponent<Rigidbody>().useGravity = true;
