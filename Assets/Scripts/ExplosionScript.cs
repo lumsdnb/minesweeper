19874f77429a5100957b1bd652851f10bc7dffec
SiHaas <simone.haas.sh@googlemail.com>
2020-01-21 12:08:02 +0100

    Added Explosion Script
    Hit Mine with Axe to Explode

---
+++ Assets/Scripts/ExplosionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+
+    public GameObject Explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision");
+        if (other.tag.Equals("mine"))
+        {
+            Debug.Log("Explosion");
+            Instantiate(Explosion, new Vector3(0, 4, 3), Quaternion.identity);
+
+        }
+    }
+}
