7e72c1490b8736d26b9eccd6551faacffa90a4b8
Jeremy Ondrey <jeremyondrey@gmail.com>
2020-01-21 10:33:37 +0100

    Renamed things

---
+++ Assets/Scripts/IsPartOf.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsPartOf : MonoBehaviour
+{
+    public GameObject motherObject;
+
+    void Update()
+    {
+        transform.position = motherObject.transform.position;
+        transform.rotation = motherObject.transform.rotation;
+    }
+}
