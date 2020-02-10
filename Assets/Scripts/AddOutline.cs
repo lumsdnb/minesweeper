58afc88b489ad338f4359d0525fcaf54897399c3
SiHaas <simone.haas.sh@googlemail.com>
2020-02-10 19:02:04 +0100

    Revert "Try to fix merge conflict"
    This reverts commit d5541edaca368aa7f3f29ab782825d6c77af6be1.

--- Assets/Scripts/AddOutline.cs
+++ Assets/Scripts/AddOutline.cs
@@ -56,7 +56,7 @@ public class AddOutline : MonoBehaviour
         Destroy(Axe.GetComponent<Outline>());
         newColor = new Color32(255, 0, 0, 255);
         LineStrenght = 10f;
-        SetUpOutline(Knife, new Color32(255, 0, 0, 255), 10f);
+        SetUpOutline(Knife, newColor, LineStrenght);
     }
     private void outlineFunction2()
     {
